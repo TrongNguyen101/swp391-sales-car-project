9c16ad91844eea66c678f6c014d833841c8a3e19
Nguyen Trong <nguyentrong.se11@gmail.com>
2025-02-01 06:16:01 +0700

    update code register page

--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -5,12 +5,16 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as authService from "../../services/AuthService";
+import * as AuthValidator from "../../validation/AuthValidation";
 
 const cx = classNames.bind(styles);
 
 /**
- * RegisterPage component renders a registration form with fields for fullname, email, password, and password confirmation.
- * It includes functionality to show/hide passwords and navigate back to the home page.
+ * RegisterPage component handles the user registration process.
+ * It includes form fields for fullname, email, password, and confirm password.
+ * It validates the input fields and displays error messages if validation fails.
+ * On successful registration, it navigates the user to the login page.
  *
  * @component
  * @example
@@ -18,11 +22,20 @@ const cx = classNames.bind(styles);
  *   <RegisterPage />
  * )
  *
- * @returns {JSX.Element} The rendered registration page component.
+ * @returns {JSX.Element} The rendered RegisterPage component.
  */
 function RegisterPage() {
   const [showPassword, setShowPassword] = useState(false);
   const [showRePassword, setShowRePassword] = useState(false);
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+  const [errorRePassword, setErrorRePassword] = useState("");
+  const [errorFullname, setErrorFullname] = useState("");
+
   const navigate = useNavigate();
 
   const handleShowPassword = () => {
@@ -38,10 +51,78 @@ function RegisterPage() {
     navigate("/");
   };
 
+  /**
+   * Handles the registration process by sending user details to the server.
+   * 
+   * @returns {Promise<void>} - A promise that resolves when the registration process is complete.
+   * 
+   * @throws {Error} - Throws an error if the registration process fails.
+   */
+  const fetchRegister = async () => {
+    try {
+      const response = await authService.postRegister(
+        fullname,
+        email,
+        password
+      );
+      if (response.status === 200) {
+        console.log(response.data);
+        alert(response.data.message);
+        navigate("/login");
+      }
+      if (response.status === 409) {
+        setErrorEmail(response.data.message);
+      }
+      if (response.status === 400) {
+        alert(response.data.message);
+      }
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response);
+      }
+    }
+  };
+
+  /**
+   * Handles the form submission event.
+   * Validates the fullname, email, password, and confirm password fields.
+   * Sets error messages for each field if validation fails.
+   * If all validations pass, triggers the register fetch request.
+   *
+   * @param {Event} event - The form submission event.
+   */
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    const fullnameError = AuthValidator.validateFullname(fullname);
+    const emailError = AuthValidator.validateEmail(email);
+    const passwordError = AuthValidator.validatePassword(password);
+    const rePasswordError = AuthValidator.validateConfirmPassword(
+      password,
+      rePassword
+    );
+
+    if (emailError) {
+      setErrorEmail(emailError);
+    }
+    if (passwordError) {
+      setErrorPassword(passwordError);
+    }
+    if (rePasswordError) {
+      setErrorRePassword(rePasswordError);
+    }
+    if (fullnameError) {
+      setErrorFullname(fullnameError);
+    }
+
+    if (!emailError && !passwordError && !rePasswordError && !fullnameError) {
+      fetchRegister();
+    }
+  };
+
   return (
     <div className={cx("container")}>
       <div className={cx("content")}>
-        <form className={cx("form")}>
+        <form className={cx("form")} onSubmit={handleOnSubmit}>
           <div className={cx("vinfast-logo")}>
             <div className={cx("logo-image")} onClick={handleBackToHome}>
               <img src="./logoJPG-removebg-preview.png" alt="vinfast-logo" />
@@ -51,31 +132,52 @@ function RegisterPage() {
             <input
               type="text"
               id="fullname"
-              className={cx("input")}
+              className={!errorFullname ? cx("input") : cx("error-input")}
               placeholder="Fullname"
               spellCheck="false"
-              required
+              value={fullname}
+              onChange={(e) => {
+                setFullname(e.target.value);
+                setErrorFullname("");
+              }}
             />
+            {errorFullname && (
+              <Typography sx={{ color: "red" }}>{errorFullname}</Typography>
+            )}
           </div>
           <div className={cx("form-group")}>
             <input
-              type="email"
+              type="text"
               id="email"
-              className={cx("input")}
+              className={!errorEmail ? cx("input") : cx("error-input")}
               placeholder="Email"
               spellCheck="false"
-              required
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                setErrorEmail("");
+              }}
             />
+            {errorEmail && (
+              <Typography sx={{ color: "red" }}>{errorEmail}</Typography>
+            )}
           </div>
           <div className={cx("field-password")}>
             <input
               type={showPassword ? "text" : "password"}
               id="password"
-              className={cx("input")}
+              className={!errorPassword ? cx("input") : cx("error-input")}
               placeholder="Password"
               spellCheck="false"
-              required
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                setErrorPassword("");
+              }}
             />
+            {errorPassword && (
+              <Typography sx={{ color: "red" }}>{errorPassword}</Typography>
+            )}
             <div className={cx("icon")}>
               <FontAwesomeIcon
                 onClick={handleShowPassword}
@@ -88,11 +190,18 @@ function RegisterPage() {
             <input
               type={showRePassword ? "text" : "password"}
               id="re-password"
-              className={cx("input")}
+              className={!errorRePassword ? cx("input") : cx("error-input")}
               placeholder="Confirm password"
               spellCheck="false"
-              required
+              value={rePassword}
+              onChange={(e) => {
+                setRePassword(e.target.value);
+                setErrorRePassword("");
+              }}
             />
+            {errorRePassword && (
+              <Typography sx={{ color: "red" }}>{errorRePassword}</Typography>
+            )}
             <div className={cx("icon")}>
               <FontAwesomeIcon
                 onClick={handleShowRePassword}
--- front-end/src/services/AuthService/index.js
+++ front-end/src/services/AuthService/index.js
@@ -1,5 +1,13 @@
 import * as request from "../../utils/AuthRequest";
 
+/**
+ * Login user with the provided details.
+ *
+ * @param {string} email - The email address of the user.
+ * @param {string} password - The password for the user's account.
+ * @returns {Promise<Object>} The response from the server.
+ * @throws {Object} The error response from the server if the request fails.
+ */
 export const postLogin = async (email, password) => {
   try {
     const response = await request.post(
@@ -19,3 +27,33 @@ export const postLogin = async (email, password) => {
     return error.response;
   }
 };
+
+/**
+ * Registers a new user with the provided details.
+ *
+ * @param {string} fullname - The full name of the user.
+ * @param {string} email - The email address of the user.
+ * @param {string} password - The password for the user's account.
+ * @returns {Promise<Object>} The response from the server.
+ * @throws {Object} The error response from the server if the request fails.
+ */
+export const postRegister = async (fullname, email, password ) => {
+  try {
+    const response = await request.post(
+      "api/Auth/Register",
+      {
+        fullname: fullname,
+        email: email,
+        password: password,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+}
